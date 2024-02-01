f2aede862f819af06babcc47a4c16d4aaf8ece1f
LetgoMoveNotLate <hj981203@gmail.com>
2024-02-01 14:13:41 +0900

    Initial commit

---
+++ src/main/java/com/example/accessingdatamysql/controller/MainController.java
@@ -0,0 +1,2 @@
+package com.example.accessingdatamysql.controller;public class MainController {
+}
---
+++ src/main/java/com/example/accessingdatamysql/repository/UserRepository.java
@@ -0,0 +1,2 @@
+package com.example.accessingdatamysql.repository;public interface UserRepository {
+}
